b6ea2fe3c9b1d0dc1d08c825c97e73a52f0ba3c8
Hina Shah <hinajoshi1084@gmail.com>
2019-10-09 09:48:16 -0400

    WIP: tag extraction works with this commit, need some cleaning up

--- src/py/create_us_tag_list.py
+++ src/py/create_us_tag_list.py
@@ -4,43 +4,98 @@ import preprocess_us
 import pytesseract
 import csv
 from matplotlib import pyplot as plt
-from PIL import Image
-from PIL import ImageFilter
+import SimpleITK as sitk
 import numpy as np
 import pprint
 
-def extractTagFromFrame(np_frame, bounding_box, tag_list):
-
-    sub_image = np_frame[bounding_box[0][1] : bounding_box[1][1],
-                         bounding_box[0][0] : bounding_box[1][0] ]
-    
-
-    config_file = '--oem 1 --psm 11'
-    sub_image = 255-sub_image
-    sub_image[sub_image<128] = 0
-    sub_image[sub_image>=128] = 255
-    img = Image.fromarray(sub_image)  
-    img.filter(ImageFilter.BLUR)
-    print('max : {}, min: {}'.format(np.amax(sub_image), np.amin(sub_image)))
-
-    data = pytesseract.image_to_data(img, 
-                                    output_type= pytesseract.Output.DICT,
-                                    config=config_file)
+def getTesseractTag(np_array, tag_list):
+    """
+    Run tesseract on an input image.
+    np_array: is supposed to be a numpy 2d array
+    tag_list: is a list of strings acceptable as tags
+    NOTE/TODO: spaces are ignored in the tag list right now. 
+    So, POST PLAC, ANT PLAC, etc are not recognized yet.
+    """
+    config_file = '--oem 1 --psm 12'
+    data = pytesseract.image_to_data(np_array, output_type= pytesseract.Output.DICT, config=config_file)
     print(data)
-
     conf = list(map(int, data['conf']))
     final_tag = 'Undecided'
     if max(conf) > 0:
         max_conf_tag_ind = conf.index(max(conf))
         tag = (data['text'][max_conf_tag_ind]).upper()
+        tag = ''.join(e for e in tag if e.isalnum())
         # does this tag live in the list?
         if tag in tag_list:
             final_tag = tag
-        elif not tag:
-            final_tag = 'No tag'
-
     print(final_tag)
     return final_tag
+
+def extractTagFromFrame(np_frame, bounding_box, tag_list):
+    """
+    Do necessary preprocessing on the numpy array, and pass it to tesseract.
+    np_fram: 2d grayscale numpy array
+    bounding_box: a list of two lists defining the upper left and lower right corners of
+    the bounding box where the tag is estimated to be. NOTE: this is very crucial here/
+    tag_list: list of acceptable tags. See the note in getTesseractTag()
+    """
+    #sub_image = np_frame[bounding_box[0][1] : bounding_box[1][1],
+    #                     bounding_box[0][0] : bounding_box[1][0] ]
+    Dimension = len(np_frame.shape)
+    # check if the input dimension is 2
+    if Dimension is not 2:
+        print('Error: Expecting a 2d image as an input to extract the tag, got another dimension, returning')
+        return None
+
+    sitk_image = sitk.GetImageFromArray(np_frame)
+    size = sitk_image.GetSize()
+    cropped_image = sitk.Crop(sitk_image, bounding_box[0],
+                [size[i] - bounding_box[1][i] for i in range(Dimension)])
+
+    plt.imshow(sitk.GetArrayFromImage(cropped_image), cmap='gray')
+    plt.pause(0.5)
+    plt.show()
+
+    # create processing configurations
+    # This looks like a bit hacky way of doing things,
+    # but a bit of experimentatio showed that there's no single way
+    # of getting to the tags. 
+    # TODO: maybe normalization/histogram equalization would help?
+    threshold_method = [0, 1] #0 for binary, 1 for otsu
+    scale = [1, 2]
+    ballsize = [0, 1, 3]
+    process_config = [ (t, s, b) for t in threshold_method 
+                                 for s in scale 
+                                 for b in ballsize  ]
+    
+    final_tag = 'Undecided'
+    fail_tag_list = ['Undecided', 'No tag']
+    # go through each one
+    for config in process_config:
+        print('Trying config: ')
+        print(config)
+        if config[0] == 0:
+            thresholded_image = (cropped_image < 128)*255
+        else:
+            thresholded_image = sitk.OtsuThreshold(cropped_image)*255
+
+        if config[1] == 1:
+            expanded_image = thresholded_image
+        else:
+            expanded_image = sitk.Expand(thresholded_image, [config[1], config[1]], sitk.sitkLinear)
+        
+        if config[2] == 0:
+            final_image = expanded_image
+        else: 
+            final_image = sitk.BinaryDilate(expanded_image, config[2], sitk.sitkBall, 0., 255.)
+        
+        final_tag = getTesseractTag(sitk.GetArrayFromImage(final_image), tag_list)
+        if final_tag not in fail_tag_list:
+            break
+    
+    return final_tag
+
+
 
 data_folder = Path("/Users/hinashah/UNCFAMLI/Data/")
 subjects = ["UNC-0414-1_20190830_111550", 
@@ -50,7 +105,7 @@ subjects = ["UNC-0414-1_20190830_111550",
             "UNC-0418-1_20190904_090711",
             "UNC-0447-1_20190927_114657"]
 tag_list_file = 'us_tags.txt'
-out_images_dir = data_folder / 'Images_1' 
+out_images_dir = data_folder / 'Images_2' 
 rescale_size = [800,600]
 
 #tag_bounding_box = [[0,60], [200,150]]
@@ -76,10 +131,16 @@ except:
 tag_statistic = dict.fromkeys(tag_list, 0)
 tag_statistic['Unknown'] = 0
 tag_statistic['Undecided'] = 0
+tag_statistic['No tag'] = 0
 
 preprocess_us.checkDir(out_images_dir, False)
+#plt.ion()
+plt.figure()
+
+
 for subject_id in subjects:
 
+    print("=========== PROCESSING SUBJECT: {} ===============".format(subject_id))
     out_dir = out_images_dir / (subject_id + "_images")
     preprocess_us.checkDir(out_dir)
     
@@ -107,7 +168,6 @@ for subject_id in subjects:
             print('========= US Model: {} was not found ========='.format(capture_model))
 
         csvfilewriter.writerow({'File': str(file_name), 'type': us_type, 'tag': tag})
-
         i+=1
     csv_file.close()
 
