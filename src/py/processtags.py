0fe6f6063b82bebf0199dfd5c693774847c72c54
Hina Shah <hinajoshi1084@gmail.com>
2019-12-04 00:58:53 -0500

    ENH: updating process tags for non-cropped images, etc

--- src/py/processtags.py
+++ src/py/processtags.py
@@ -20,7 +20,7 @@ def checkDir(dir_path, delete=True):
 def main(args):
     data_folder = Path(args.dir)
     out_folder = Path(args.out_dir)
-    checkDir(out_folder)
+    #checkDir(out_folder)
 
     #  Setup logging:
     loglevel = logging.DEBUG if args.debug else logging.INFO
@@ -59,9 +59,12 @@ def main(args):
             cropped = None
             if jpg_file_name.exists():
                 simage = sitk.ReadImage(str(jpg_file_name))
-                size = simage.GetSize()
-                cropped = sitk.Crop(simage, bounding_box[0],
-                            [size[i] - bounding_box[1][i] for i in range(2)])
+                if args.crop_images:        
+                    size = simage.GetSize()
+                    cropped = sitk.Crop(simage, bounding_box[0],
+                                [size[i] - bounding_box[1][i] for i in range(2)])
+                else:
+                    cropped = simage
 
             tag_folder = out_folder/tag
             target_simlink_name = tag_folder/file_name
@@ -78,6 +81,11 @@ def main(args):
             logging.info('Copying file: {} -> {}'.format(file, target_simlink_name))
             
             try:
+                # TODO: WARNING: the replacement hardcoding should be eventually by creating a more consistent
+                # way of saving the images in the first place
+                if file.find('/mnt/') >= 0:
+                    som_dir = args.som_home_dir + '/' if args.som_home_dir[-1] != '/' else args.som_home_dir
+                    file = file.replace('/mnt/', args.som_home_dir)
                 shutil.copyfile(file, target_simlink_name)
             except FileNotFoundError:
                 logging.warning("Couldn't find file: {}".format(file))
@@ -97,6 +105,8 @@ if __name__=="__main__":
     parser.add_argument('--out_dir', type=str, help='Output directory location.')
     parser.add_argument('--debug', action='store_true', help='Add debug info in log')
     parser.add_argument('--tags', type=str, help='Space delimited list of tag files to be copied')
+    parser.add_argument('--crop_images', action='store_true', help='Crop images while copying them over')
+    parser.add_argument('--som_home_dir', type=str, help='SOM server directory, needed to replace a mount position in the file names')
     args = parser.parse_args()
 
     main(args)
