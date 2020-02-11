a92350dbb10f4c20a5a77baf6d4256105e1506a1
Hina Shah <hinajoshi1084@gmail.com>
2020-02-10 23:16:41 -0500

    ENH: Scripts to prepare data for HC synthesis from blindsweeps

--- src/py/correct_study_tag_structures.py
+++ src/py/correct_study_tag_structures.py
@@ -5,14 +5,14 @@ import os
 import csv
 import sys
 import utils
-import shutil
+
 
 def main(args):
     data_folder = Path(args.dir)
     out_folder = Path(args.out_dir)
 
     #  Setup logging:
-    utils.setupLogFile(out_dir, args.debug)
+    utils.setupLogFile(out_folder, args.debug)
     
     none_tags = ['Unknown', 'Undecided', 'No tag']
     
@@ -21,34 +21,43 @@ def main(args):
     for tag_file in tag_file_names:
         logging.info('--- PROCESSING: {}'.format(tag_file))
         # Read the tags
-        tags = []
         this_none_tags = []
+        file_tag_pairs = []
         try:
             with open(tag_file) as f:
                 csv_reader = csv.DictReader(f)
                 file_tag_pairs = [ (line['File'], line['type'], line['tag']) for line in csv_reader]
-                tags = [ line['tag'] for line in csv_reader ]
-                this_none_tags = [pair[2] for pair in tags if pair[2] in none_tags]
+                this_none_tags = [pair[2] for pair in file_tag_pairs if pair[2] in none_tags]
         except (OSError) as e:
             logging.error('Error reading csv file: {}'.format(tag_file))
             continue
         
-        if len(tags) > 0 and len(tags) == len(this_none_tags):
+        if len(file_tag_pairs) == 0:
+            logging.warning('Did not find any information in: {}'.format(str(tag_file)))
+            continue
+
+        if len(file_tag_pairs) == len(this_none_tags):
         # If all are unknown, then delete the info.csv file, we will come back to it at a later 
         # stage
-            shutil.remove(tag_file)
+            logging.info('No known tags in: {}'.format(str(tag_file)))
+            #tag_file.unlink()
         else:
             # change the mount destination in file names
             csvrows = []
-            for pair in tags:
-                pair['File'] = (pair['File']).replace('/mnt', args.out_dir)
-                csvrows.append({'File':pair['File'], 'type':pair['type'], 'tag':pair['tag']})
-            csv_file = open(str(out_dir / 'info.csv'), 'w')
-            field_names = ['File', 'type', 'tag']
-            csvfilewriter = csv.DictWriter(csv_file, field_names)
-            csvfilewriter.writeheader()
-            csvfilewriter.writerows(csvrows) 
-            csv_file.close()
+            for pair in file_tag_pairs:
+                fn = (pair[0]).replace('/mnt', args.som_home_dir)
+                tp = pair[1]
+                tg = pair[2]
+                csvrows.append({'File':fn, 'type':tp, 'tag':tg})
+            logging.info('Correcting to: {}, with number: {}'.format(tag_file, len(csvrows)))
+            if len(csvrows) == len(file_tag_pairs):
+                dir_name = tag_file.parent
+                csv_file = open(str(dir_name/'info_corrected.csv'), 'w')
+                field_names = ['File', 'type', 'tag']
+                csvfilewriter = csv.DictWriter(csv_file, field_names)
+                csvfilewriter.writeheader()
+                csvfilewriter.writerows(csvrows) 
+                csv_file.close()
 
     logging.info('----- DONE -----')
 
--- src/py/prepare_data_lstm_hc.py
+++ src/py/prepare_data_lstm_hc.py
@@ -10,8 +10,95 @@ import concurrent.futures
 import time
 import utils
 import json
+from dump_images_from_cine import dumpImagesForCine
+import subprocess
+import shutil
+import SimpleITK as sitk
+
+def runClassificationOnFolder(data_folder, path_to_classification, out_folder, 
+                                class_name, prediction_threshold=0.9, 
+                                    sitk_reference_image_path=None):
+    """
+    Run a node-based classification script on the input folder.
+    The classification (javascript) script's source code is at: https://github.com/juanprietob/us-famli-nn
+    Input parameters:
+    data_folder : Points to the directory that has the image dump for all the frames of a cine (basically a folder of images)
+    path_to_classification: Points to the directory where us-famli-nn has been set up
+    out_folder: Points to the output directory. a prediction_<data_folder_name>.csv file will be written out here.
+    class_name: Name of the class to look for in the generated predictions file
+    prediction_threshold: probability threshold to keep/copy the images
+    """
+    node_source_path = path_to_classification + '/us-famli-nn/bin/index.js'
+    if not Path(node_source_path).exists():
+        logging.error('Source code for classification does not exist at: {}'.format(node_source_path))
+        return 0
+    
+    output_file = str(out_folder / ('prediction_' + data_folder.stem + '.csv'))
+    command_list = ['node', node_source_path, '--dir', str(data_folder), '--type', 'remove_calipers', '--type', 'classify_ga', '--out', output_file]
+    try:
+        subprocess.run(command_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+    except Exception as e:
+        logging.error(e)
+        logging.info('Error processing image dump folder: {}'.format(data_folder))
+        return 0
+    
+    class_images = []
+    try:
+        with open(output_file) as f:
+            # Read prediction.csv file
+            csv_reader = csv.DictReader(f)
+            # Keep files with prediction > threshold and class class_nam
+            class_images = [line['img'] for line in csv_reader if  \
+                            line['class'] == class_name and \
+                            float(line[class_name]) > prediction_threshold]
+    except Exception as e:
+        logging.error(e)
+        logging.info('Error processing the prediction file {}'.format(output_file))
+        return 0
+
+    if len(class_images) > 0:
+        copy_dest_dir = out_folder / 'cine_class_images'
+        utils.checkDir(copy_dest_dir, False)
+        if sitk_reference_image_path is not None:
+            ref_img = sitk.ReadImage(str(sitk_reference_image_path))
+            ref_spacing = ref_img.GetSpacing()[:2]
+            ref_direction = ref_img.GetDirection()
+            target_direction = [ref_direction[0], ref_direction[1], ref_direction[3], ref_direction[4]]
+            target_size = ref_img.GetSize()[:2]
+
+        for image_path_str in class_images:
+            image_path = Path(image_path_str)
+            dest_file_path = str(copy_dest_dir / (data_folder.stem + '_' + image_path.name))
+            if sitk_reference_image_path is not None:            
+                src_image = sitk.ReadImage(image_path_str)
+                if src_image.GetDimension() == 3:
+                    logging.warning('GOT a 2d image with dimension 3 {}'.format(src_img.GetSize()))
+
+                sitk_resample = sitk.ResampleImageFilter()
+                sitk_resample.SetOutputSpacing(ref_spacing)
+                sitk_resample.SetOutputDirection(target_direction)
+                sitk_resample.SetSize(target_size)
+                sitk_out_img = sitk_resample.Execute(src_image)
+                sitk.WriteImage(sitk_out_img, dest_file_path)
+                del src_image
+                del sitk_out_img
+                del sitk_resample
+            else:
+                shutil.copyfile(image_path_str, dest_file_path)
+    return len(class_images)
 
 def getStudyOutputFolder(study, data_folder, out_parent_folder):
+    """
+    Creates a path for a study that is relative to the output parent directory.
+    This is useful when copying the tree structure of the studies
+
+    For example:
+    study = /a/b/c/Ultrasounds/2019-10/UNC-001
+    data_folder = /a/b/c
+    out_parent_folder = /d/e/f/g
+
+    this returns: /d/e/f/g/Ultrasounds/2019-10/UNC-001
+    """
     subject_id = study.name
     study_path = study.parent
     study_path_relative = study_path.relative_to(data_folder)
@@ -30,13 +117,14 @@ def main(args):
                     'L15', 'L45', 'L0', 'L1', 'M0', 'M1', 'RTA', 'RTB', 'RTC']
     tag_2d = ['HC']
 
-    info_csv_list = list( data_folder.glob('**/info.csv') )
-    logging.info('Found {} studies with an info.csv '.format(len(info_csv_list)))
-    print('Found {} studies with an info.csv '.format(len(info_csv_list)))
-
+    info_csv_list = list( data_folder.glob('**/info_corrected.csv') )
+    logging.info('Found {} studies with an info_corrected.csv '.format(len(info_csv_list)))
+    print('Found {} studies with an info_corrected.csv '.format(len(info_csv_list)))
+    img_count_db = {}
     bs_db = {}
     counter = 0
     for tag_file in info_csv_list:
+        print('-- Found: {} --- studies'.format(counter))
         logging.info('--- PROCESSING: {}'.format(tag_file))
         try:
             with open(tag_file) as f:
@@ -48,21 +136,48 @@ def main(args):
                         bs_list.append({'File': line['File'], 'tag': line['tag']})
                     elif line['tag'] in tag_2d and line['type'] == '2d image':
                         im_list.append({'File': line['File']})
-                logging.info('BS LIST')
-                logging.info(bs_list)
-                logging.info('IM LIST')
-                logging.info(im_list)
+                logging.debug('BS LIST')
+                logging.debug(bs_list)
+                logging.debug('IM LIST')
+                logging.debug(im_list)
                 if len(bs_list) > 0 and len(im_list) > 0:
                     study_name = (tag_file.parent).stem
-                    study_det = {}
-                    study_det['blindsweeps'] = bs_list
-                    study_det['hc_2d'] = im_list
-                    bs_db[study_name] = study_det
+                    study_detail = {}
+                    study_detail['blindsweeps'] = bs_list
+                    study_detail['hc_2d'] = im_list
+                    bs_db[study_name] = study_detail
+                    # Create the study directory in the output folder
+                    out_study_dir = getStudyOutputFolder(tag_file.parent, data_folder, out_images_dir)
+                    utils.checkDir(out_study_dir, False)
+                    logging.debug('Output folder is: {}'.format(out_study_dir))
+                    hc_img_path = Path(args.som_working_dir + '/' + im_list[0]['File'])
+                    # Iterate through the blindsweeps list
+                    num_imgs_found = 0
+                    for bs_item in bs_list:
+                        # Dump images of the blindsweep -> in the form of nrrd's (not jpgs)
+                        img_dump_dir = out_study_dir/'ImgDump'
+                        utils.checkDir(img_dump_dir, False)
+                        logging.debug('Dumping images for cine: {}'.format(bs_item['File']))
+                        cine_dump_path = dumpImagesForCine( Path( args.som_working_dir + '/' + bs_item['File']), img_dump_dir)
+                        # Run classification on the imgdump folder
+                        logging.debug('Run classification for cine: {}'.format(bs_item['File']))
+                        num_imgs_found += runClassificationOnFolder(Path(cine_dump_path), args.path_to_classification, Path(out_study_dir), \
+                                                                    class_name='head', sitk_reference_image_path=hc_img_path)
+                    logging.debug('Number of head images found in blindsweeps: {}'.format(num_imgs_found))
+                    img_count_db[str(out_study_dir)] = num_imgs_found
+                    # Copy the first head images to a 2d_class_images
+                    logging.debug('Copying the first HC file: {}'.format(hc_img_path))
+                    shutil.copyfile(hc_img_path, out_study_dir/'head_2d_image.dcm')
+                    #Delete the imgdump folder
+                    shutil.rmtree(img_dump_dir)
                     counter+=1
         except (OSError) as e:
             logging.error('Error reading csv file: {}'.format(tag_file))
             return
-
+        except Exception as e:
+            logging.error('Error in processing the tag file: {}'.format(e))
+    
+    # Write the list of HC and blindsweep images
     try:
         j_f = json.dumps(bs_db, indent=4)
         f = open(str(out_images_dir/'hc_and_blindsweeps.json'),"w")
@@ -73,18 +188,29 @@ def main(args):
         logging.error('Error dumping the json file')
         return
 
+    # Write out the number of images identified in each study:
+    try:
+        j_f = json.dumps(img_count_db, indent=4)
+        with open(str(out_images_dir/'study_images_count.json'),"w") as f:
+            f.write(j_f)
+    except Exception as e:
+        print(e)
+        logging.error('Error dumping the image count file')
+
     logging.info('Found {} studies with HC files'.format(counter))
     print('Found {} studies with HC files'.format(counter))
     print('------DONE-----------')
 
-
 if __name__=="__main__":
 
     parser = ArgumentParser()
-    parser.add_argument('--dir', type=str, help='Directory with study folders that have an info.csv file generated')
+    parser.add_argument('--dir', type=str, help='Directory with study folders that have an info.csv file generated', required=True)
     parser.add_argument('--out_dir', type=str, help='Output directory location.'
-                'The directory hierarchy will be copied to this directory')
+                                'The directory hierarchy will be copied to this directory', required=True)
     parser.add_argument('--debug', action='store_true', help='Add debug info in log')
+    parser.add_argument('--som_working_dir', type=str, help = 'Mount location for the SOM server.' 
+                                'Basically parent location for the famli folder, and original ultrasound images are stored here', required=True)
+    parser.add_argument('--path_to_classification', type=str, help='Path to the classification code : us-famli-nn/bin/index.js')
     parser.add_argument('--use_threads', action='store_true', help='Use threads to run the code')
     args = parser.parse_args()
 
