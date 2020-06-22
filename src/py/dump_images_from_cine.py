6c974ee92783eae77fa5f3af739160d9e6da2f94
Hina Shah <hinajoshi1084@gmail.com>
2020-06-22 11:50:55 -0400

    ENH: Adding mp4 image dumps, adding an option for a verbatim logging

--- src/py/dump_images_from_cine.py
+++ src/py/dump_images_from_cine.py
@@ -6,22 +6,79 @@ import logging
 import pydicom
 import numpy as np
 import SimpleITK as sitk
+import cv2
 
-def dumpImagesForCine(cine_file_path, out_dir, store_jpg = False, frame_list = None):
+def dumpImagesForCineMP4(path_to_video, dump_dir, frame_list=None, verbatim=True):
+    msg=''
+    try:
+        cap = cv2.VideoCapture(str(path_to_video))
+        # Check if camera opened successfully
+        if (cap.isOpened() is False): 
+            msg = "Error opening video stream or file"
+            raise IOError(msg)
+
+        if frame_list is None:
+            frame_list = range(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        # Read until video is completed
+        success,image = cap.read()
+        frame_num = 0
+        while success:
+            if frame_num in frame_list:
+                out_frame_path = dump_dir/("frame_{}.jpg".format(frame_num))
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                cv2.imwrite( str(out_frame_path), gray)
+                frame_list.remove(frame_num)
+                if len(frame_list)==0:
+                    break
+            # Capture frame-by-frame
+            success, image = cap.read()
+            frame_num+=1    
+        # When everything done, release the video capture object
+        cap.release()
+    except Exception as e:
+        msg = "Error dumping mp4 image frames: {}".format(e)
+        if verbatim:
+            logging.warning(msg)
+    finally:
+        return msg
+
+def dumpImagesForCine(cine_file_path, out_dir, store_jpg = False, frame_list = None, verbatim=True):
+    msg = ''
     if not cine_file_path.exists():
-        logging.warning('PATH: {} does not exist'.format(str(cine_file_path)))
-        return
+        msg =  'PATH: {} does not exist'.format(str(cine_file_path))
+        if verbatim:
+            logging.debug(msg)
+        return None, msg
+    out_path = out_dir/cine_file_path.stem
+    utils.checkDir(out_path, False)
+
+    # Check if this is a dicom or an mp4 file:
+    if (cine_file_path.suffix).lower() == '.mp4':
+        msg = dumpImagesForCineMP4(path_to_video=cine_file_path, dump_dir=out_path, frame_list=frame_list)
+        return out_path, msg
+
+
+    if (cine_file_path.suffix).lower() not in ['.dcm', '.dicom']:
+        msg = 'Unsupported video type during frame dump, returning'
+        if verbatim:
+            logging.debug(msg)
+        return None, msg
 
     file_str = str(cine_file_path)
     # read the metadata header
     ds = pydicom.read_file(file_str)
     if ds is None:
-        logging.warning('File: {} Missing DICOM metadata'.format(file_str))
+        msg = 'File: {} Missing DICOM metadata'.format(file_str)
+        if verbatim:
+            logging.debug(msg)
+        return None, msg
 
     sopclass = ds['0008', '0016'].value
     if sopclass != '1.2.840.10008.5.1.4.1.1.3.1':
-        logging.warning('File: {} not a cine'.format(file_str) )
-        return
+        msg = 'File: {} not a cine'.format(file_str)
+        if verbatim:
+            logging.debug(msg)
+        return None, msg
 
     # Get numpy arra
     np_image = ds.pixel_array
@@ -37,9 +94,6 @@ def dumpImagesForCine(cine_file_path, out_dir, store_jpg = False, frame_list = N
     ds.decompress()
 
     num_frames = ds['0028', '00008'].value
-    out_path = out_dir/cine_file_path.stem
-    utils.checkDir(out_path, False)
-
     if frame_list is None:
         frame_list = range(num_frames)
 
@@ -49,13 +103,14 @@ def dumpImagesForCine(cine_file_path, out_dir, store_jpg = False, frame_list = N
             preprocessus.writeImage(np_image[i, :, :], file_out )
         file_out = str(out_path) + '/frame_' + str(i) + '.nrrd'
         sim = sitk.GetImageFromArray(np_image[i,:,:])
-        sim.SetSpacing(ds.PixelSpacing)
+        if ['0028','0030'] in ds:
+            sim.SetSpacing(ds.PixelSpacing)
         sitk.WriteImage(sim, file_out)
         del sim
 
     del ds
     del np_image
-    return out_path
+    return out_path, msg
 
 def main(args):
     data_folder = Path(args.dir)
