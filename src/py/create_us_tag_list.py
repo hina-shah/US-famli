76fba094075a2c70eda41b6dba42cc0daf96c45c
Hina Shah <hinajoshi1084@gmail.com>
2020-12-05 14:29:21 -0500

    ENH: Major changes to add a greedy mode, using new libraries

--- src/py/create_us_tag_list.py
+++ src/py/create_us_tag_list.py
@@ -21,9 +21,14 @@ import logging
 import sys
 import gc
 import concurrent.futures
-import preprocessus
-import extracttagtext
 import time
+import json
+import numpy as np
+
+from FamliImgIO import dcmio
+from FamliFileIO import taginfo
+from FamliOCR import famlitesseract as tess
+import extracttagtext
 import utils
 
 def getStudyOutputFolder(study, data_folder, out_parent_folder):
@@ -33,64 +38,104 @@ def getStudyOutputFolder(study, data_folder, out_parent_folder):
     out_dir = out_parent_folder  / study_path_relative / subject_id
     return out_dir
 
-def extractTagForStudy(study, data_folder, out_images_dir, tag_list, non_tag_us, tag_bounding_box):
-    tag_statistic = dict.fromkeys(tag_list, 0)
-    tag_statistic['Unknown'] = 0
-    tag_statistic['Undecided'] = 0
-    tag_statistic['No tag'] = 0
+def extractImageArrayFromUS(file_path, out_dir=None):
+    dcmobj = dcmio.DCMIO(file_path)
+    # Create the output path
+    out_path = None
+    if out_dir is not None:
+        out_path = out_dir / (file_path.stem + '.jpg')
+    
+    np_frame = dcmobj.get_repres_frame(out_path)
+    us_type = dcmobj.get_type()
+    us_model = dcmobj.get_model()
+
+    return np_frame, us_type, us_model
+
+def extractTagForStudy(study, data_folder, out_images_dir, tag_list, non_tag_us, 
+                        tag_bounding_box, server_path, greedy=False):
     
     logging.info("=========== PROCESSING SUBJECT: {} ===============".format(study.name))
     out_dir = getStudyOutputFolder(study, data_folder, out_images_dir)
-    utils.checkDir(out_dir)
+    utils.checkDir(out_dir, delete = (not greedy))
     
+    # look for the info.csv file in the output folder. 
+    # if exists, read the file names and the tags
+     
     i=1
-    file_names = list( study.glob('**/*.dcm') )
+    file_paths = list( study.glob('**/*.dcm') )
     csvrows=[]
     unknown = 0
-    for file_name in file_names:
+    tag_manager = taginfo.TagInfoFile(out_dir)
+    tag_statistic = {}
+    prev_tags = {}
+    if greedy and tag_manager.exists():
+        # If this is in greedy mode, try to get the names of the files
+        # that have an undecided tag:
+        tag_manager.read()
+        # get a copy of the previously created tags for all files
+        prev_tags = tag_manager.getDictWithNameKeys()
+        # clear the tags stored in the file. (This does not delete the file, just empties the data structure)
+        tag_manager.clear()
+        tag_manager.deleteTagFile()
 
-        start = time.time()
-        logging.debug("FILE {}: {}".format(i, str(file_name)))
-        logging.debug(str(file_name))
-        np_frame, us_type, capture_model = preprocessus.extractImageArrayFromUS(file_name, out_dir, None)
-        end = time.time()
-        logging.debug('Preprocessing took : {} seconds'.format(end-start))
-        
-        if len(capture_model)>0 and capture_model not in tag_bounding_box.keys():
-            logging.warning('US Model: {} not supported for file: {}'.format(capture_model, file_name))
-            del np_frame
+    for file_path in file_paths:
+        logging.debug("FILE {}: {}".format(i, str(file_path)))
+
+        # Make sure path exists
+        if not file_path.exists():
+            logging.warning('File: {} does not exist, skipping'.format(file_path))
             continue
 
-        # Extract text from the image
-        start = time.time()
-        tag = 'Unknown'
-        if np_frame is not None and \
-            us_type not in non_tag_us and\
-            capture_model in tag_bounding_box.keys():
-            # Run tag extraction
-            tag = extracttagtext.extractTagFromFrame(np_frame, tag_bounding_box[capture_model], tag_list)
-        end = time.time()
-        logging.debug('Tag extraction took : {} seconds'.format(end-start))
-        tag_statistic[tag] += 1
-        del np_frame
-        
-        if tag in ['Unknown', 'Undecided', 'No tag']:
-            unknown += 1
+        # if in greedy mode, look at the tag, look at the tag and decide if we wamt to extract tag
+        getTag = True
+        tag = None
+        us_type = None
+        if greedy and len(prev_tags) > 0:
+            name = file_path.name
+            if name in prev_tags:
+                tag = prev_tags[name][1]
+                us_type = prev_tags[name][0]
+                if tag in ['Unknown', 'Undecided', 'No tag']:
+                    getTag = True
+                else:
+                     getTag = False
 
-        csvrows.append({'File': str(file_name), 'type': us_type, 'tag': tag})
+        if getTag:
+            start = time.time()
+            # Get the representative frame
+            np_frame, us_type, capture_model = extractImageArrayFromUS(file_path, out_dir=out_dir)
+            end = time.time()
+            logging.debug('Preprocessing took : {} seconds'.format(end-start))
+            if len(capture_model)>0 and capture_model not in tag_bounding_box.keys():
+                logging.warning('US Model: {} not supported for file: {}'.format(capture_model, file_path))
+                del np_frame
+                continue
+            # Extract the tag
+            start = time.time()
+            tag = 'Unknown'
+            if np_frame is not None and \
+                us_type not in non_tag_us and \
+                capture_model in tag_bounding_box.keys():
+                # Run tag extraction
+                tag = tess.extractTagFromFrame(np_frame, tag_bounding_box[capture_model], tag_list)
+            end = time.time()
+            logging.debug('Tag extraction took : {} seconds'.format(end-start))
+            del np_frame
+            if tag in ['Unknown', 'Undecided', 'No tag']:
+                unknown += 1
+        else:
+            logging.debug('Skipping the file: {}, tag: {}, type: {}, because it was known'.format(file_path, tag, us_type))
+
+        tag_manager.addTag(file_path.parent, server_path, file_path.name, us_type, tag, write=True)
         i+=1
         gc.collect()
 
-    # Write the csv file
-    if len(csvrows) > 0 and (unknown < len(csvrows)):
-        csv_file = open(str(out_dir / 'info.csv'), 'w')
-        field_names = ['File', 'type', 'tag']
-        csvfilewriter = csv.DictWriter(csv_file, field_names)
-        csvfilewriter.writeheader()
-        csvfilewriter.writerows(csvrows) 
-        csv_file.close()
+    tag_statistic = tag_manager.tag_statistic
+    # If all unknown, delete the tag file. 
+    if unknown == len(file_paths):
+        tag_manager.deleteTagFile()
+        tag_manager.clear()
     return tag_statistic
-
 
 def main(args):
     data_folder = Path(args.dir)
@@ -107,7 +152,12 @@ def main(args):
     logging.info('Found {} studies '.format(len(studies)))
     print('Found {} studies '.format(len(studies)))
     
-    tag_list_file = 'us_tags.txt'
+    # read the list of acceptable tags in the ultrasound file
+    tag_list = utils.getTagsList()
+    tag_statistic = dict.fromkeys(tag_list, 0)
+    tag_statistic['Unknown'] = 0
+    tag_statistic['Undecided'] = 0
+    tag_statistic['No tag'] = 0
     
     # Approximate bounding box of where the tag is written acoording to the 
     # us model
@@ -115,28 +165,15 @@ def main(args):
                          'LOGIQe':  [[0,55], [200,160]],
                          'Voluson S': [[40,75], [255,190]],
                          'LOGIQeCine': [[0,0],[135,90]],
-                         'Turbo': [[75,20,],[230,80]]
+                         'Turbo': [[75,20,],[230,80]],
+                         'Voluson E8': [[40,75], [255,250]]
                         }
 
     # list of ultrasound image types whose tags we do not care about right now.
-    non_tag_us = ['Unknown', 'ge kretz image', 'Secondary capture image report',
+    non_tag_us = ['Unknown', 'Secondary capture image report',
                     'Comprehensive SR', '3D Dicom Volume']
 
-    # read the list of acceptable tags in the ultrasound file
-    tag_list = None
-    try:
-        with open(Path(__file__).parent / tag_list_file) as f:
-            tag_list = f.read().splitlines()
-    except:
-        logging.warning('ERROR READING THE TAG FILE')
-        tags = ['M', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'R15', 'R45', 'R0', 'RO', 'R1',
-                    'L15', 'L45', 'L0', 'LO', 'L1', 'M0', 'M1', 'RTA', 'RTB', 'RTC', 
-                    '3D1', 'RLQ', 'RUQ', 'LUQ', 'LLQ', 'EQ']
-    tag_statistic = dict.fromkeys(tag_list, 0)
-    tag_statistic['Unknown'] = 0
-    tag_statistic['Undecided'] = 0
-    tag_statistic['No tag'] = 0
-
+    
     # Also read in study directories that might have been finished by a previous run - do not want to rerun them again
     finished_study_file = out_images_dir/'finished_studies.txt'
     finished_studies = None
@@ -153,14 +190,17 @@ def main(args):
             try:
                 infocsv_dir = getStudyOutputFolder(Path(study), data_folder, out_images_dir)
                 logging.info('Opening: {}'.format(infocsv_dir))
-                with open(infocsv_dir/'info.csv', 'r') as f:
-                    csv_reader = csv.DictReader(f)
-                    for line in csv_reader:
-                        tag_statistic[line['tag']] += 1
+                tag_file_man =taginfo.TagInfoFile(infocsv_dir)
+                tag_file_man.read()
+                if tag_file_man.getNumFiles() > 0:
+                    for tag in tag_file_man.tag_statistic:
+                        if tag not in tag_statistic:
+                            tag_statistic[tag] = 0
+                        tag_statistic[tag] += tag_file_man.tag_statistic[tag]        
             except (OSError, ValueError) as err:
-                logging.warning('Error reading previously created info.csv for subject: {}: {}'.format(study, err))
+                logging.warning('Error reading previously created tags.csv for subject: {}: {}'.format(study, err))
             except:
-                logging.warning('Error reading previously created info.csv for subject: {}'.format(study))
+                logging.warning('Error reading previously created tags.csv for subject: {}'.format(study))
                 logging.warning('Unknown except while reading csvt: {}'.format(sys.exc_info()[0]))
     else:
         cleaned_studies = studies
@@ -171,7 +211,8 @@ def main(args):
             # Start the load operations and mark each future with its URL
             future_tags = {executor.submit(extractTagForStudy, study, 
                                             data_folder, out_images_dir, tag_list,
-                                            non_tag_us, tag_bounding_box): study for study in cleaned_studies}
+                                            non_tag_us, tag_bounding_box, 
+                                            Path(args.server_path), args.greedy ): study for study in cleaned_studies}
             for future in concurrent.futures.as_completed(future_tags):
                 d = future_tags[future] 
                 logging.info('Finished processing: {}'.format(d))
@@ -185,7 +226,8 @@ def main(args):
         i=1
         for study in cleaned_studies:
             this_tag_statistic = extractTagForStudy(study, data_folder, out_images_dir, 
-                                                    tag_list, non_tag_us, tag_bounding_box)
+                                                    tag_list, non_tag_us, tag_bounding_box, 
+                                                    Path(args.server_path), args.greedy)
             logging.info('Finished processing: {}'.format(study))
             for key, value in this_tag_statistic.items():
                 tag_statistic[key] += value
@@ -196,6 +238,8 @@ def main(args):
             i+=1
     
     pprint(tag_statistic)
+    with open(out_images_dir/"NumberOfTags.json", "w") as outfile:
+        json.dump(tag_statistic, outfile, indent=4) 
     logging.info(pformat(tag_statistic))
     logging.info('---- DONE ----')
     print('------DONE-----------')
@@ -205,11 +249,13 @@ if __name__=="__main__":
     
     parser = ArgumentParser()
     parser.add_argument('--dir', type=str, help='Directory with subject subfolders.'
-                'Every lowest level subfolder will be considered as a study')
+                'Every lowest level subfolder will be considered as a study', required=True)
     parser.add_argument('--out_dir', type=str, help='Output directory location.'
-                'The directory hierarchy will be copied to this directory')
+                'The directory hierarchy will be copied to this directory', required=True)
+    parser.add_argument('--server_path', type=str, help='Path to the server relative to which teh paths of files will be saved')
     parser.add_argument('--debug', action='store_true', help='Add debug info in log')
     parser.add_argument('--use_threads', action='store_true', help='Use threads to run the code')
+    parser.add_argument('--greedy', action='store_true', help='If given, will retry to extract tags for files that were processed previously')
     args = parser.parse_args()
 
     main(args)
