1e5b9ceec211f16e319d163dc05127c7d789dc07
Hina Shah <hinajoshi1084@gmail.com>
2021-06-17 11:20:16 -0400

    ENH: Adding support for MPEG4 compression format in dicoms

--- src/py/FamliImgIO/dcmio.py
+++ src/py/FamliImgIO/dcmio.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import logging
-
+import tempfile
+import shutil
 import pydicom
 import SimpleITK as sitk
 import numpy as np
+import cv2
 
 class DCMIO:
     '''
@@ -30,7 +32,7 @@ class DCMIO:
             self.file_path = filepath
         else:
             raise TypeError(('DICOM input File path object is of the wrong type'))
-        
+
         # check the suffix
         if self.file_path.suffix.lower() not in ['.dcm', '.dicom']:
             raise TypeError('DICOM input File extension is of the wrong type')
@@ -43,7 +45,7 @@ class DCMIO:
         self.ds = pydicom.read_file(file_str)
         if self.ds is None:
             raise IOError('File: {} Missing DICOM metadata'.format(file_str))
-        
+
         # Set various information values
         self.set_dsdata()
 
@@ -75,12 +77,12 @@ class DCMIO:
         self.us_model = ''
         if ['0008', '1090'] in self.ds:
             self.us_model = self.ds['0008', '1090'].value
-        
+
         if self.us_type == 'cine' and self.us_model=='LOGIQe':
             self.us_model = 'LOGIQeCine'
 
         self.photometric_interpretation = ''
-        if ['0028','0004'] in self.ds: 
+        if ['0028','0004'] in self.ds:
             self.photometric_interpretation = self.ds['0028','0004'].value
 
     # GETTERS
@@ -97,7 +99,7 @@ class DCMIO:
         return self.sopclass
 
     def check_photometric_interpretation(self):
-        if self.photometric_interpretation not in ['RGB', 'YBR_FULL_422', 'MONOCHROME2']:
+        if self.photometric_interpretation not in ['RGB', 'YBR_FULL_422', 'MONOCHROME2', 'YBR_PARTIAL_420']:
             logging.warning('UNSUPPORTED PHOTOMETRIC INTERPRETATION: ' + self.photometric_interpretation)
             return False
         else:
@@ -105,7 +107,44 @@ class DCMIO:
 
     def cleanup(self):
         del self.ds
-    
+
+    def read_mp4(self):
+        pixel_data = (self.ds).PixelData
+        # find 'ftyp' in pixel data, go back by 4 bytes, and
+        if len(pixel_data) < 104:
+            logging.warning('Length of pixel data is suspicious for mp4 data')
+            return None
+
+        start_index = pixel_data.find(b'ftyp')
+        if start_index < 0:
+            logging.warning('Did not find beginning of the header in the mp4 video')
+            return None
+        temp_dir = tempfile.gettempdir()
+        temp_file_path = Path(temp_dir)/(self.file_path.name).replace(self.file_path.suffix, '.mp4')
+
+        try:
+            mp4_video = pixel_data[(start_index-4):]
+            # save binary data to temp folder
+            with open(temp_file_path, 'wb') as f:
+                f.write(mp4_video)
+            cap = cv2.VideoCapture(str(temp_file_path))
+            success = cap.isOpened()
+            if not success:
+                raise IOError("Error opening the temp MP4 file")
+            frames = []
+            while success:
+                success,frame = cap.read()
+                if frame is not None:
+                    frames.append(frame)
+            all_frames = np.stack(frames, axis=0)
+        except Exception as e:
+            logging.warning("Error reading the dicom file as mp4: Error: \n {}".format(e))
+            all_frames = None
+        finally:
+            if temp_file_path.exists():
+                temp_file_path.unlink()
+            return all_frames
+
     # Get numpy array for the image
     def get_gray_nparray(self):
         if self.ds is  None:
@@ -114,24 +153,34 @@ class DCMIO:
         if not self.check_photometric_interpretation():
             logging.warning('Unsupported photometric interpretation')
             return None
-
-        np_image = self.ds.pixel_array
-        if self.us_type == '2d image':
-            if self.photometric_interpretation == "RGB":
-                np_image = np.dot(np_image, [0.2989, 0.5870, 0.1140])
-                np_image = np.squeeze(np_image.astype(np.uint8))
-            elif self.photometric_interpretation == "YBR_FULL_422":
-                np_image = np.squeeze(np_image[:,:,0])
-            elif self.photometric_interpretation == "MONOCHROME2":
-                np_image = np.squeeze(np_image)
-        elif self.us_type == 'cine':
-            if self.photometric_interpretation in ["YBR_FULL_422", "RGB"]:
-                # Grab the Y or R channel of the image.
-                # NOTE: for RGB image, it is assumed that the labels are in the "yellow channel", so grabbing R channel would suffice
-                np_image =  np_image[:, :, :, 0]
+        if self.us_type == 'cine' and self.photometric_interpretation in ['YBR_PARTIAL_420']:
+            np_image = self.read_mp4()
+            if np_image is not None:
+                # Will get the green channel here, this photometric interpretation was found in Butterfly images
+                # The tags seem to be in the green channel for butterfly cines.
+                np_image = np_image[:, :, :, 1]
         else:
-            logging.debug('DICOM type: {}, cant get nparray of this'.format(self.us_type))
-            return None
+            try:
+                np_image = self.ds.pixel_array
+                if self.us_type == '2d image':
+                    if self.photometric_interpretation == "RGB":
+                        np_image = np.dot(np_image, [0.2989, 0.5870, 0.1140])
+                        np_image = np.squeeze(np_image.astype(np.uint8))
+                    elif self.photometric_interpretation == "YBR_FULL_422":
+                        np_image = np.squeeze(np_image[:,:,0])
+                    elif self.photometric_interpretation == "MONOCHROME2":
+                        np_image = np.squeeze(np_image)
+                elif self.us_type == 'cine':
+                    if self.photometric_interpretation in ["YBR_FULL_422", "RGB"]:
+                        # Grab the Y or R channel of the image.
+                        # NOTE: for RGB image, it is assumed that the labels are in the "yellow channel", so grabbing R channel would suffice
+                        np_image =  np_image[:, :, :, 0]
+                else:
+                    logging.debug('DICOM type: {}, cant get nparray of this'.format(self.us_type))
+                    np_image = None
+            except Exception as e:
+                logging.warning("Error reading pixel data: \n".format(e))
+                np_image = None
         
         return np_image
 
@@ -162,7 +211,11 @@ class DCMIO:
             return None
         
         # Get a mid-cine frame
-        middle_frame = int(self.ds['0028', '00008'].value/2)
+        if ['0028', '0008'] in self.ds:
+            middle_frame = int(self.ds['0028', '0008'].value/2)
+        else:
+            logging.warning("Number of frames attribute not found in dicom")
+            middle_frame = 0
 
         # Maybe cine not of value if only one frame exists, return
         if middle_frame == 0:
